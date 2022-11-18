9b09fdc71c6566d39430108ff14a8393b1ade1de
Elkhan2003 <boss.armsport@gmail.com>
2022-11-18 18:07:13 +0600

    Restyling Update ♻️

--- Elcho/js/script.js
+++ Elcho/js/script.js
@@ -1,1 +1,8 @@
-
+$(window).scroll(function () {
+	let sc = $(window).scrollTop();
+	if (sc > 70) {
+		$("#header__scroll").addClass("small");
+	} else {
+		$("#header__scroll").removeClass("small");
+	}
+});
