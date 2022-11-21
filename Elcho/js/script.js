cef61b86223aec43fc064f662a138d7ea5c85107
Elkhan2003 <boss.armsport@gmail.com>
2022-11-21 08:19:59 +0600

    Global update to portfolio ⚡️

--- Elcho/js/script.js
+++ Elcho/js/script.js
@@ -1,8 +1,67 @@
-$(window).scroll(function () {
-	let sc = $(window).scrollTop();
-	if (sc > 70) {
-		$("#header__scroll").addClass("small");
-	} else {
-		$("#header__scroll").removeClass("small");
-	}
+// // ! Sticky Navigation Menu JS Code
+// let header = document.querySelector("#header__scroll");
+// window.onscroll = function () {
+// 	if (document.documentElement.scrollTop > 20) {
+// 		header.classList.add("small");
+// 	} else {
+// 		header.classList.remove("small");
+// 	}
+// };
+
+// !
+$(function () {
+	// ! Sticky Header on Scroll
+	$(window).scroll(function () {
+		let sc = $(window).scrollTop();
+		if (sc > 50) {
+			$("#header__scroll").addClass("small");
+		} else {
+			$("#header__scroll").removeClass("small");
+		}
+	});
+
+	// ! Smooth scroll
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data("scroll"),
+			blockOffset = $(blockId).offset().top;
+
+		$("#nav a").removeClass("active");
+		$this.addClass("active");
+
+		$("html, body").animate(
+			{
+				scrollTop: blockOffset,
+			},
+			1000
+		);
+	});
+
+	// ! Menu nav toggle
+	$("#nav_toggle").on("click", function (event) {
+		event.preventDefault();
+
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	});
+
+	// ! Collapse
+	$("[data-collapse]").on("click", function (event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data("collapse");
+
+		$this.toggleClass("active");
+	});
+
+	// ! Slider
+	$("[data-slider]").slick({
+		infinite: true,
+		fade: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
 });
