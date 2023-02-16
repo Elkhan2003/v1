0173964afd80f52a9a512fd07db76c02559b0fd6
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2023-02-16 17:38:58 +0600

    Fix bug

--- Elcho/js/script.js
+++ Elcho/js/script.js
@@ -19,37 +19,86 @@ window.onscroll = function () {
 
 // !
 
-// ! JQuery
-$(function () {
-	// ! Smooth scroll
-	$(document).ready(function () {
-		$('.nav a span[data-scroll^="#"]').click(function () {
-			let target = $(this).attr("data-scroll");
-			$("html, body").animate(
-				{
-					scrollTop: $(target).offset().top - 70,
-				},
-				500
-			);
-			$('.nav a span[data-scroll^="#"]').removeClass("active");
-			$(this).addClass("active");
-			return false;
+// // ! JQuery
+// $(function () {
+// 	// ! Smooth scroll
+// 	$(document).ready(function () {
+// 		$('.nav a span[data-scroll^="#"]').click(function () {
+// 			let target = $(this).attr("data-scroll");
+// 			$("html, body").animate({
+// 					scrollTop: $(target).offset().top - 70,
+// 				},
+// 				500
+// 			);
+// 			$('.nav a span[data-scroll^="#"]').removeClass("active");
+// 			$(this).addClass("active");
+// 			return false;
+// 		});
+// 	});
+
+// 	// ! scroll top
+// 	$(function () {
+// 		$("#scroll_top").click(function () {
+// 			$("html, body").animate({
+// 				scrollTop: 0
+// 			}, 200);
+// 			return false;
+// 		});
+// 	});
+// });
+
+// ! Smooth scroll
+function initSmoothScroll() {
+	let navLinks = document.querySelectorAll('.nav a span[data-scroll^="#"]');
+	navLinks.forEach(function (navLink) {
+		navLink.addEventListener("click", function (event) {
+			event.preventDefault();
+			let target = this.getAttribute("data-scroll");
+			window.scrollTo({
+				top: document.querySelector(target).offsetTop - 70,
+				behavior: "smooth"
+			});
+			navLinks.forEach(function (link) {
+				link.classList.remove("active");
+			});
+			this.classList.add("active");
 		});
 	});
+}
 
-	// ! scroll top
-	$(function () {
-		$("#scroll_top").click(function () {
-			$("html, body").animate({ scrollTop: 0 }, 200);
-			return false;
-		});
-	});
+// ! scroll top
+// function initScrollTop() {
+// 	document.getElementById("scroll_top").addEventListener("click", function () {
+// 		document.querySelector("html, body").animate({
+// 			scrollTop: 0
+// 		}, 200);
+// 		return false;
+// 	});
+// }
 
-	// ! Menu nav toggle
-	$("#nav_toggle").on("click", function (event) {
+// ! menu
+function initMenu() {
+	const navToggle = document.getElementById("nav_toggle");
+	const nav = document.getElementById("nav");
+	const links = nav.getElementsByTagName("a");
+
+	navToggle.addEventListener("click", function (event) {
 		event.preventDefault();
-
-		$(this).toggleClass("active");
-		$("#nav").toggleClass("active");
+		navToggle.classList.toggle("active");
+		nav.classList.toggle("active");
 	});
+
+	for (let i = 0; i < links.length; i++) {
+		links[i].addEventListener("click", function () {
+			navToggle.classList.remove("active");
+			nav.classList.remove("active");
+		});
+	}
+}
+
+// ! initialize all functions on page load
+document.addEventListener("DOMContentLoaded", function () {
+	initSmoothScroll();
+	initMenu();
+	// initScrollTop();
 });
